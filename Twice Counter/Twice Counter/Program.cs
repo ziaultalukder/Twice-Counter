50cb5df680877b651be2131f62190b7de6e52b0d
ziaul2023 <ziaul@primotechltd.com>
2024-10-20 12:18:53 +0600

    problem solve letcode

---
+++ Twice Counter/Twice Counter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twice_Counter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] list = { "Tom", "Jerry", "Tom", "Thomas", "Jerry", "Courage", "Tom", "Courage" };
+            int twiceResult = TwiceCounter(list);
+            Console.WriteLine(twiceResult);
+            Console.ReadLine();
+        }
+        static int TwiceCounter(string[] strings)
+        {
+            Dictionary<string, int> map = new Dictionary<string, int>();
+            int count = 0;
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (map.ContainsKey(strings[i]))
+                {
+                    map[strings[i]]++;
+                }
+                else
+                {
+                    map.Add(strings[i], 1);
+                }
+            }
+
+            foreach (var author in map)
+            {
+                if (author.Value == 2)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
